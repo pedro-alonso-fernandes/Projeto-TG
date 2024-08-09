25ecfbb7d4249eab0b36a2f45ed67f393c101491
Gabriel <gabrielmendesdesouza123@gmail.com>
2024-08-09 20:10:16 -0300

    09.08.2024

---
+++ Sistema-TG/src/controller/GerarPdf.java
@@ -0,0 +1,130 @@
+package controller;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.ResultSet;
+import java.text.DateFormat;
+import java.util.Date;
+
+import javax.swing.text.StyleConstants.ColorConstants;
+
+import org.w3c.dom.html.HTMLFieldSetElement;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.TabStop;
+import com.itextpdf.text.TabStop.Alignment;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class  GerarPdf {
+	public GerarPdf() {
+
+		Document document = new Document();
+		// gera o pdf
+		try {
+			
+			
+			PdfWriter.getInstance(document, new FileOutputStream("Atiradores.pdf"));
+			
+			document.open();
+			
+
+			Date data = new Date();
+			DateFormat formatador = DateFormat.getDateInstance(DateFormat.DATE_FIELD);
+			String Data1 = "Data: ";
+			Font font = FontFactory.getFont(FontFactory.TIMES_BOLD, 16, BaseColor.BLACK);
+			Font font2 = FontFactory.getFont(FontFactory.TIMES_BOLD, 14, BaseColor.BLACK);
+			Image imagem = Image.getInstance("file:///C:/Users/gabri/Desktop/Projeto-TG/Sistema-TG/src/model/images/Ex.png");
+			Paragraph paragrafo1 = new Paragraph("Todos os Atiradores do Tiro de Guerra - 01-013", font);
+			imagem.scalePercent(75);
+			paragrafo1.add(imagem);
+			paragrafo1.setAlignment(Element.ALIGN_CENTER);
+			document.add(paragrafo1);
+			document.add(new Paragraph("     "));
+			document.add(new Paragraph( Data1 + formatador.format(data) , font));
+			document.add(new Paragraph("     "));
+			Paragraph paragrafo2 = new Paragraph("Atiradores Cadastrados" , font);
+			paragrafo2.setAlignment(Element.ALIGN_CENTER);
+			document.add(paragrafo2);
+			document.add(new Paragraph("     "));
+			document.add(new Paragraph("     "));
+			document.add(new Paragraph("     "));
+			// TABELA Atiradores
+
+			PdfPTable tabela = new PdfPTable(5);
+			Paragraph paraid = new Paragraph("ID" , font2);
+			PdfPCell coll1 = new PdfPCell(paraid);
+			tabela.addCell(coll1).setHorizontalAlignment(1);
+			PdfPCell coll2 = new PdfPCell(new Paragraph("Nome" , font2));
+			tabela.addCell(coll2).setHorizontalAlignment(1);
+			PdfPCell coll3 = new PdfPCell(new Paragraph("Nome de Guerra" , font2));
+			tabela.addCell(coll3).setHorizontalAlignment(1);
+			PdfPCell coll4 = new PdfPCell(new Paragraph("Cargo" , font2));
+			tabela.addCell(coll4).setHorizontalAlignment(1);
+			PdfPCell coll5 = new PdfPCell(new Paragraph("QTD-Guardas" , font2));
+			tabela.addCell(coll5).setHorizontalAlignment(1);
+			
+			
+			ResultSet rs = AtiradorDAO.getAtiradores();
+			String id = "0";
+			String qtd = "0";
+			try {
+				
+				while(rs.next()){
+					if (rs.getInt("id") < 10) {
+						PdfPCell colid = new PdfPCell(new Paragraph(String.valueOf(id + rs.getInt("id"))));
+						tabela.addCell(colid).setHorizontalAlignment(1);
+						PdfPCell colnome = new PdfPCell(new Paragraph(new Paragraph (rs.getString("nome"))));
+						tabela.addCell(colnome).setHorizontalAlignment(1);
+						PdfPCell colguerra = new PdfPCell(new Paragraph(new Paragraph (rs.getString("guerra"))));
+						tabela.addCell(colguerra).setHorizontalAlignment(1);
+						PdfPCell colcargo = new PdfPCell(new Paragraph(new Paragraph (rs.getString("cargo"))));
+						tabela.addCell(colcargo).setHorizontalAlignment(1);
+						PdfPCell colqtd = new PdfPCell(new Paragraph(new Paragraph (qtd)));
+						tabela.addCell(colqtd).setHorizontalAlignment(1);
+					}
+					else {
+						PdfPCell colid = new PdfPCell(new Paragraph(String.valueOf(rs.getInt("id"))));
+						tabela.addCell(colid).setHorizontalAlignment(1);
+						PdfPCell colnome = new PdfPCell(new Paragraph(new Paragraph (rs.getString("nome"))));
+						tabela.addCell(colnome).setHorizontalAlignment(1);
+						PdfPCell colguerra = new PdfPCell(new Paragraph(new Paragraph (rs.getString("guerra"))));
+						tabela.addCell(colguerra).setHorizontalAlignment(1);
+						PdfPCell colcargo = new PdfPCell(new Paragraph(new Paragraph (rs.getString("cargo"))));
+						tabela.addCell(colcargo).setHorizontalAlignment(1);
+						PdfPCell colqtd = new PdfPCell(new Paragraph(new Paragraph (qtd)));
+						tabela.addCell(colqtd).setHorizontalAlignment(1);
+					}
+					
+					
+				}
+				
+			} catch (Exception e3) {
+				System.out.println(e3);
+			}
+
+			document.add(tabela);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			document.close();
+		}
+
+		// abre o pdf no leitor padrão
+		try {
+			Desktop.getDesktop().open(new File("Atiradores.pdf"));
+		} catch (Exception e2) {
+			System.out.println(e2);
+		}
+	}
+}
--- Sistema-TG/src/view/telaAtirador.java
+++ Sistema-TG/src/view/telaAtirador.java
@@ -15,6 +15,7 @@ import javax.swing.table.DefaultTableModel;
 
 import controller.AtiradorDAO;
 import controller.Conexao;
+import controller.GerarPdf;
 
 import java.awt.Color;
 import javax.swing.JButton;
@@ -66,7 +67,7 @@ public class telaAtirador extends JFrame {
 		contentPane.setLayout(null);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 10, 552, 444);
+		scrollPane.setBounds(10, 10, 552, 426);
 		contentPane.add(scrollPane);
 
 		table = new JTable();
@@ -126,7 +127,7 @@ public class telaAtirador extends JFrame {
 				Cadastro.setVisible(true);
 			}
 		});
-		btnNewButton.setBounds(10, 464, 114, 21);
+		btnNewButton.setBounds(10, 465, 114, 21);
 		contentPane.add(btnNewButton);
 
 		JButton btnNewButton_1 = new JButton("Editar");
@@ -138,7 +139,7 @@ public class telaAtirador extends JFrame {
 				dispose();
 			}
 		});
-		btnNewButton_1.setBounds(133, 464, 114, 21);
+		btnNewButton_1.setBounds(136, 465, 114, 21);
 		contentPane.add(btnNewButton_1);
 
 		JButton btnNewButton_2 = new JButton("Remover");
@@ -150,10 +151,10 @@ public class telaAtirador extends JFrame {
 			}
 		});
 		btnNewButton_2.setFont(new Font("Arial Black", Font.BOLD, 12));
-		btnNewButton_2.setBounds(257, 464, 114, 21);
+		btnNewButton_2.setBounds(260, 465, 114, 21);
 		contentPane.add(btnNewButton_2);
 
-		JButton btnNewButton_3 = new JButton("Voltar ao Menu");
+		JButton btnNewButton_3 = new JButton("Menu");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
@@ -162,8 +163,20 @@ public class telaAtirador extends JFrame {
 			}
 		});
 		btnNewButton_3.setFont(new Font("Arial Black", Font.BOLD, 12));
-		btnNewButton_3.setBounds(381, 464, 164, 21);
+		btnNewButton_3.setBounds(384, 465, 114, 21);
 		contentPane.add(btnNewButton_3);
+		
+		JButton PDF = new JButton("");
+		PDF.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GerarPdf pdf = new GerarPdf();
+				
+			}
+		});
+		PDF.setToolTipText("GerarpdfAtdr");
+		PDF.setIcon(new ImageIcon(telaAtirador.class.getResource("/model/images/business (1).png")));
+		PDF.setBounds(508, 446, 40, 40);
+		contentPane.add(PDF);
 
 		this.setLocationRelativeTo(null);
 	}
